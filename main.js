b8fedc50960a65ccc9d22336afe98c6b2e5a0058
Theee Sheeep <79360988+thatonesheep80@users.noreply.github.com>
2021-12-18 17:26:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+scoreRightWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload()
 {
 }
@@ -9,7 +13,8 @@ function setup() {
 	video = createCapture(VIDEO);
     video.hide();
   
-  poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on("pose", gotPoses);
 }
 
 function modelLoaded()
@@ -17,6 +22,29 @@ function modelLoaded()
   console.log("model loaded!");
 }
 
-function draw() {
-	game()
+function gotPoses(results)
+{
+    
+    if (results.length > 0)
+    {
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + ", rightWristY = " + rightWristY);
+    }
+}
+
+function draw() 
+{
+	image(video, 0, 0, 600, 500);
+
+ 	if(scoreRightWrist > 0.2)
+	{
+	 fill("#FF0000");
+     stroke("#FF0000");
+     circle(rightWristX, rightWristY, 20);
+	}
 }
